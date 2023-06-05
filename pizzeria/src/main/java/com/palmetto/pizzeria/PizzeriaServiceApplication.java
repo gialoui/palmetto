035407ea5104684e8c5009f349a5d32fb5adf69a
KhangNDD <nddkhang.2909@gmail.com>
2023-06-05 15:42:09 +0700

    feat: Init codebase

---
+++ pizzeria/src/main/java/com/palmetto/pizzeria/PizzeriaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.palmetto.pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaServiceApplication.class, args);
+	}
+
+}
---
+++ pizzeria/src/test/java/com/palmetto/pizzeria/PizzeriaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palmetto.pizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
